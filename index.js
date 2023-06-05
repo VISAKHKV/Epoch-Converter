9cba4547d6b3e5e4a91de232c64708652e731d13
visakh <visakh@NTP-LAP-766.chn.nestgroup.net>
2023-06-05 16:12:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+let months = [
+  "january",
+  "Feruary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function logSubmit(event) {
+  console.log("submitted");
+
+  const time = document.getElementById("time").value;
+  const turn = document.querySelector("#turn").checked;
+
+  let date;
+  if (turn == true) {
+    date = new Date((parseInt(time) - 19800) * 1000);
+  } else {
+    date = new Date(parseInt(time) * 1000);
+  }
+
+  let dateStr =
+    date.getDate() +
+    " - " +
+    months[date.getMonth() + 1] +
+    " - " +
+    date.getFullYear();
+
+  let timeStr =
+    date.getHours() +
+    " hrs " +
+    date.getMinutes() +
+    " mins " +
+    date.getSeconds() +
+    " secs";
+  console.log("date : ", dateStr);
+
+  let display = document.getElementById("display");
+  if (time == "") {
+    display.innerHTML = "Give a valid input...";
+  } else {
+    display.innerHTML = "Date : " + dateStr + "<br />" + "Time : " + timeStr;
+  }
+
+  event.preventDefault();
+}
+
+const form = document.getElementById("real_time");
+
+form.addEventListener("submit", (event) => {
+  // handle the form data
+  logSubmit(event);
+});
